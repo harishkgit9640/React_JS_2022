f8091e865fe5fd4a6119d3e01862d3c192e96561
Harish Kumar <harishstudio877@gmail.com>
2022-08-14 17:29:07 +0530

    I have completed 8th video of Reactjs

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -12,20 +12,31 @@ export default function TextForm(props) {
     }
 
     const handleOnChange = (event) => {
-        console.log("On change");
         settext(event.target.value);
     }
 
     const [text, settext] = useState("Enter text here...");
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" onChange={handleOnChange} rows="8" value={text} ></textarea>
+        <>
+            <div className="container">
+                <div>
+                    <h1>{props.heading}</h1>
+                    <div className="mb-3">
+                        <textarea className="form-control" onChange={handleOnChange} rows="8" value={text} ></textarea>
+                    </div>
+                    <button className="btn btn-primary" onClick={handleUpCase}>ToUpperCase</button>
+                    <button className="btn btn-success ms-2" onClick={handleLoCase}>ToLowerCase</button>
+                </div>
             </div>
-            <button className="btn btn-primary" onClick={handleUpCase}>ToUpperCase</button>
-            <button className="btn btn-success ms-2" onClick={handleLoCase}>ToLowerCase</button>
-        </div>
+
+            <div className="container my-3">
+                <p> {text.split(" ").length} Words and {text.length} Character </p>
+                <p> {0.008 * text.split(" ").length} Minutes to Read </p>
+                <h2>Preview</h2>
+                <p className='text-justify'> {text} </p>
+            </div>
+
+        </>
     );
 
 }
